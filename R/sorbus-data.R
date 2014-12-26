6aff1e2ae352539e9914a2368d25e652927f9f24
santiagombv <santiagombv@gmail.com>
2014-12-26 20:57:40 -0300

    description of sorbus dataset

---
+++ R/sorbus-data.R
@@ -0,0 +1,27 @@
+#' Phenotypic and fitness meassures in Sorbus terminalis
+#' 
+#' Data from \emph{Sorbus terminalis} (population Hoyos de Muñoz), including
+#' measures from seven floral traits, plant size and plant fitness. Variables
+#' are PL, petal length; CD, corolla diameter; SL, style length; HK, herkogamy;
+#' HL, hypanthium length; HW, hypanthium width; GW, gynoecium width; BASAR,
+#' basal area (plant size) and W, fitness measured as fruit production.
+#' 
+#' This dataset was kindly provided by Mariano Ordano and co-workers. Please, 
+#' cite as Ordano et al. 2008 (see references).
+#' 
+#' @docType data
+#'   
+#' @usage data(sorbus)
+#'   
+#' @format a data set with 27 rows and 9 variables.
+#'   
+#' @references Ordano, M., Fornoni, J., Boege, K. and Domínguez, C.A. 2008. The 
+#'   adaptive value of phenotypic floral integration. New Phytologist 
+#'   179:1183–1192.
+#'   
+#' @examples
+#' data(sorbus)
+#' 
+#' @name sorbus
+#'   
+NULL
