e2d3ee122d7375e6b504d4633d73e3b74b833795
santiagombv <santiagombv@gmail.com>
2015-06-20 00:57:42 -0300

    erasing non-ASCI characters

--- R/givensrot.R
+++ R/givensrot.R
@@ -19,27 +19,27 @@
 #' @author Santiago Benitez-Vieyra, based in the SAS routine of Wicklin (2013).
 #' 
 #' @references Bendel, R.B. and Mickey, M.R. 1978. Population correlation matrices for sampling
-#' experiments. Communications in Statistics—Simulation and Computation 7:163–182.
+#' experiments. Communications in Statistics-Simulation and Computation 7:163-182.
 #' @references Davies, P.I. and Higham, N.J. 2000. Numerically stable generation of correlation
-#' matrices and their factors. BIT 40:640–651.
+#' matrices and their factors. BIT 40:640-651.
 #' @references Wicklin, R. 2013. Simulating data with SAS. Cary, NC:SAS Institute Inc.
 #' 
 #' @export
 #'  
 givensrot <- function(M, i, j){
-  Aii <- M[i,i]
-  Aij <- M[i,j]
-  Ajj <- M[j,j]
-  t <- (Aij + sqrt(Aij^2 - (Aii-1)*(Ajj-1))) / (Ajj - 1)
+  Mii <- M[i,i]
+  Mij <- M[i,j]
+  Mjj <- M[j,j]
+  t <- (Mij + sqrt(Mij^2 - (Mii-1)*(Mjj-1))) / (Mjj - 1)
   c <- 1/sqrt(1+t^2)
   s <- c*t
-  Ai <- A[i,]
-  Aj <- A[j,] 
-  A[i,] = c*Ai - s*Aj
-  A[j,] = s*Ai + c*Aj
-  Ai <- A[,i]
-  Aj <- A[,j]
-  A[,i] <- c*Ai - s*Aj
-  A[,j] <- s*Ai + c*Aj
-  A
+  Mi <- M[i,]
+  Mj <- M[j,] 
+  M[i,] = c*Mi - s*Mj
+  M[j,] = s*Mi + c*Mj
+  Mi <- M[,i]
+  Mj <- M[,j]
+  M[,i] <- c*Mi - s*Mj
+  M[,j] <- s*Mi + c*Mj
+  M
 }
